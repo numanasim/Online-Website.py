8264bc29c6b31ae63bd2cd6699f274719347de38
numanasim <118437644+numanasim@users.noreply.github.com>
2024-03-20 15:32:06 +0530

    Add files via upload

---
+++ pynew.py
@@ -0,0 +1,10 @@
+userReply = input("Would you like to buy stamps, buy an envelope, or make a copy? (Enter stamps, envelope, or copy) ")
+if userReply == "stamps":
+    print("We have many stamp designs to choose from.")
+elif userReply == "envelope":
+    print("We have many envelope sizes to choose from.")
+elif userReply == "copy":
+    copies = input("How many copies would you like? (Enter a number) ")
+    print("Here are {} copies.".format(copies))
+else:
+    print("We currently don't sell {} .Thank you, please visit again.".format(userReply))
